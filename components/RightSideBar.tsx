8b8ad96e81a82f4d70a4c5990a98dee79d2366d7
BaharAbbasiDelvand <145874091+BaharAbbasiDelvand@users.noreply.github.com>
2024-07-28 18:38:23 -0400

    implemented color picker+font changer+export to pdf

--- components/RightSideBar.tsx
+++ components/RightSideBar.tsx
@@ -1,29 +1,31 @@
-import React from "react";
+import React, { useRef } from "react";
 import Dimensions from "./settings/Dimensions";
 import Text from "./settings/Text";
 import Color from "./settings/Color";
 import Export from "./settings/Export";
 import { RightSidebarProps } from "@/types/type";
 import { modifyShape } from "@/lib/shapes";
+import { fabric } from "fabric";
 
 const RightSideBar = ({elementAttributes, setElementAttributes, fabricRef, activeObjectRef, isEditingRef, syncShapeInStorage}:RightSidebarProps) => {
-
+  const colorInputRef = useRef(null); 
+  const strokeInputRef = useRef(null); 
   const handleInputChange=(property:string, value: string)=>{
     if(!isEditingRef.current) isEditingRef.current = true
     setElementAttributes((prev)=>({
       ...prev, [property]: value
     }))
-    modifyShape({canvas: fabricRef.current as fabric, property, value, activeObjectRef, syncShapeInStorage})
+    modifyShape({canvas: fabricRef.current as fabric.Canvas, property, value, activeObjectRef, syncShapeInStorage})
   }
     return (
         <section className="flex flex-col border-t border-primary-grey-100 bg-primary-black text-primary-grey-300 min-2-[227px] sticky right-0 h-full max-sm:hidden select-none">
             <h3 className="px-5 pt-4 text-xs uppercase">Design</h3>
             <span className="text-xs text-primary-grey-300 mt-3 px-5 border-b border-primary-grey-100 pb-4">Make any change you like!</span>
             <Dimensions width={elementAttributes.width} height={elementAttributes.height} handleInputChange={handleInputChange} isEditingRef={isEditingRef}/>
-            <Text/>
-            <Color/>
-            <Color />
-            <Export/>
+            <Text fontFamily={elementAttributes.fontFamily} fontSize={elementAttributes.fontSize} fontWeight={elementAttributes.fontWeight} handleInputChange={handleInputChange}/>
+            <Color inputRef={colorInputRef} attribute={elementAttributes.fill} attributeType="fill" placeholder="color" handleInputChange={handleInputChange}/>
+            <Color inputRef={strokeInputRef} attribute={elementAttributes.stroke} attributeType="stroke" placeholder="color" handleInputChange={handleInputChange}/>
+            <Export />
         </section>
     );
 };
